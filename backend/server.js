b52710183dfb88377db467b38dcfc00f9700c3eb
tomitoton <tomleo785@gmail.com>
2026-06-25 13:25:29 -0300

    git commit -m
    Nuñez, Navarro - Implementación de operaciones ABM para gestión de ambientes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-//endpoint ambientes
 const express = require("express");
 const cors = require("cors");
 const db = require("./config/db");
@@ -8,25 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+// GET AMBIENTES
 app.get("/api/ambientes", (req, res) => {
-  db.query(
-    "SELECT * FROM ambientes",
-    (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json(err);
-      }
-
-      res.json(results);
+  db.query("SELECT * FROM ambientes", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json(err);
     }
-  );
+
+    res.json(results);
+  });
 });
 
-app.listen(3001, () => {
-  console.log("Servidor funcionando en puerto 3001");
-});
 
-//endpoint mobiliario
+// GET MOBILIARIO
 app.get("/api/mobiliario", (req, res) => {
   db.query("SELECT * FROM mobiliario", (err, results) => {
     if (err) {
@@ -35,4 +30,86 @@ app.get("/api/mobiliario", (req, res) => {
 
     res.json(results);
   });
+});
+
+
+// AGREGAR AMBIENTES
+app.post("/api/ambientes", (req, res) => {
+  const {
+    nombre,
+    tipo,
+    metros_cuadrados,
+    cantidad_muebles,
+    estado
+  } = req.body;
+
+  db.query(
+    "INSERT INTO ambientes (nombre, tipo, metros_cuadrados, cantidad_muebles, estado) VALUES (?, ?, ?, ?, ?)",
+    [nombre, tipo, metros_cuadrados, cantidad_muebles, estado],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        mensaje: "Ambiente agregado"
+      });
+    }
+  );
+});
+
+
+// MODIFICAR AMBIENTES
+app.put("/api/ambientes/:id", (req, res) => {
+  const {
+    nombre,
+    tipo,
+    metros_cuadrados,
+    cantidad_muebles,
+    estado
+  } = req.body;
+
+  db.query(
+    "UPDATE ambientes SET nombre=?, tipo=?, metros_cuadrados=?, cantidad_muebles=?, estado=? WHERE id=?",
+    [
+      nombre,
+      tipo,
+      metros_cuadrados,
+      cantidad_muebles,
+      estado,
+      req.params.id
+    ],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        mensaje: "Ambiente actualizado"
+      });
+    }
+  );
+});
+
+
+// BORRAR AMBIENTES
+app.delete("/api/ambientes/:id", (req, res) => {
+  db.query(
+    "DELETE FROM ambientes WHERE id=?",
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      res.json({
+        mensaje: "Ambiente eliminado"
+      });
+    }
+  );
+});
+
+
+app.listen(3001, () => {
+  console.log("Servidor funcionando en puerto 3001");
 });
